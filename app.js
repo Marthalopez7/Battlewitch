175af44b792d98dc01f57529d7c30c2823c0726b
Marthalopez7 <116187271+Marthalopez7@users.noreply.github.com>
2023-03-07 10:40:23 -0800

    testing branch
    testing diff ways to build actual game

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 const rows = 10;
 const col = 10;
 
-let gameboard = document.getElementById("playingBoard")
+// links to the html div element
+let gameboard = document.getElementById("playingBoard");
+
+// grid 2 of each witch type 0 = nothing, 1 = witch, 2 = damaged witch, 3 = witch ded, 4 = potion, 5 = miss
+let gridBoard = [
+    [0,1,1,0,1,1,0,0,0,0]
+    [0,0,0,0,0,0,0,0,1,0]
+    [0,0,1,1,0,0,0,0,0,0]
+    [0,0,1,1,0,0,0,1,0,0]
+    [0,0,0,0,0,0,0,1,0,0]
+    [0,0,0,1,0,0,0,1,1,0]
+    [0,1,0,0,0,0,0,0,0,0]
+    [0,1,0,0,0,1,1,0,0,0]
+    [0,1,1,0,0,1,1,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0]
+];
 
 
 
-
-
-
-
+// click a sqaure to launch potion bottle
 element.addEventListener('click', (e) => launchpotion(e.target)) 
 
 function launchpotion(e){
 
 }
+
