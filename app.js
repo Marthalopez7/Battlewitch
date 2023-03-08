d1c3200b6f20dbdd7321cc5a3febe3d3a9f6e26e
Marthalopez7 <116187271+Marthalopez7@users.noreply.github.com>
2023-03-08 10:10:17 -0800

    Updates js, html and css
    Added multiplayer and single player html pages, buttons (center them), and added click event listener

--- app.js
+++ app.js
@@ -24,7 +24,21 @@ let gridBoard = [
 // click a sqaure to launch potion bottle
 element.addEventListener('click', (e) => launchpotion(e.target)) 
 
-function launchpotion(e){
+function launchpotion(event){
 
 }
+
+
+// when user clicks a sqaure and it hits, the color of the sqaure changes it to red
+document.addEventListener("click", hitWitch, false);
+
+function hitWitch() {
+    document.pbs.style.backgroundcolor = '#512525';
+}
+
+// document.addEventListener("click", hitWitch, true);
+
+// function hitWitch() {
+//     document.body.style.backgroundImage = 'potion';
+// }
 
